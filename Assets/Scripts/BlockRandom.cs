8f3ffe998f0bca07e1b5f32d8b97a07b9a5d7a0a
LaithJebril <56149154+LaithJebril@users.noreply.github.com>
2020-08-02 02:35:35 +0300

    Brick Textures Added

---
+++ Assets/Scripts/BlockRandom.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockRandom : MonoBehaviour
+{
+    public Texture[] BlockTextures;
+    void Start()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().material.SetTexture("_BaseMap", BlockTextures[Random.Range(0, BlockTextures.Length)]);
+    }
+
+
+}
